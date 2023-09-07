a1398164d8e372fe7753b55cdce359226cb76213
doongjohn <doongjohn@gmail.com>
2023-09-07 21:19:46 +0900

    update

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,11 @@
+#include <format>
 #include <iostream>
 
 #include "hello.hpp"
 
 auto main() -> int {
+  std::cout << std::format("This is {} {}\n", "C++", 20);
+
 #ifdef HELLO
   hello();
 #endif
