78cbbc9ad898b337be910b866b5bd93792342233
doongjohn <doongjohn@gmail.com>
2023-10-31 06:01:08 +0900

    update

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,13 @@
 #include <format>
 #include <iostream>
 
+#ifdef HELLO
 #include "hello.hpp"
+#endif
 
 auto main() -> int {
-  std::cout << std::format("This is {} {}\n", "C++", 20);
+  const auto str = std::string("C++");
+  std::cout << std::format("This is {} {}\n", str, 20);
 
 #ifdef HELLO
   hello();
